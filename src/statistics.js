1242777f19d2fb5c97756090fde8da648a4f3f38
Avi Romanoff <avi@romanoff.me>
2017-02-27 16:22:12 -0500

    Improve statistics

--- src/statistics.js
+++ src/statistics.js
@@ -1,9 +1,9 @@
 import _ from 'lodash'
 import moment from 'moment'
-import EventTypes from './EventTypes.js'
+import ET from './EventTypes.js'
 import F from 'lodash/fp'
 
-const millisToMinutes = num => num / 60 * 1000
+// const millisToMinutes = num => num / 60 * 1000
 // const millisToRoundedMinutes = F.compose(F.round(2), millisToMinutes)
 // const millisToRoundedMinutes = num => _.round(millisToMinutes(num), 2)
 const millisToRoundedMinutes = num => _.round(num / (60 * 1000), 2)
@@ -11,8 +11,8 @@ const millisToRoundedMinutes = num => _.round(num / (60 * 1000), 2)
 const rowTypeIs = eventType => F.compose(F.isEqual(eventType), F.get('type'))
 
 const doesEventTypeBreakSilence = type => _.includes([
-  EventTypes.TEACHER_TALK_START,
-  EventTypes.STUDENT_TALK_START
+  ET.TEACHER_TALK_START,
+  ET.STUDENT_TALK_START,
 ], type)
 
 const isSilentBetween = F.every(row => !doesEventTypeBreakSilence(row.type))
@@ -26,67 +26,79 @@ const isSilentBetween = F.every(row => !doesEventTypeBreakSilence(row.type))
 // the row as soon as it fails a check. That is, if the row makes it through
 // all the checks without causing the function to return, it is section we care
 // about.
-function getWaitTimeOnes(rows, requireQuestionEvent = false) {
+function waitTimeOne(rows) {
 
-  let waitTimes = []
+  let waitTimes = {
+    a: {
+      content: [],
+      nonContent: [],
+    },
+    b: {
+      content: [],
+      nonContent: [],
+    },
+  }
 
   rows.forEach((row, i) => {
-    const teacherStartTalking = (row.type === EventTypes.TEACHER_TALK_START)
-
     // Check #1: Make sure teacher started talking
-    if (! teacherStartTalking) { return }
+    const teacherStartTalking = (row.type === ET.TEACHER_TALK_START)
+    const indexOfTeacherStopTalking = _.findIndex(rows, rowTypeIs(ET.TEACHER_TALK_END), i)
+    if (!teacherStartTalking) { return }
 
-    const indexOfTeacherTalkResume = _.findIndex(rows, rowTypeIs(EventTypes.TEACHER_TALK_START), i+1)
-    const doesTeacherTalkResume = (indexOfTeacherTalkResume !== -1)
+    // Check #2: Make sure teacher or student eventually starts talking again between
+    // the end of
+    const indexOfNextTalkStart = _.findIndex(rows, r => (r.type === ET.TEACHER_TALK_START) || (r.type === ET.STUDENT_TALK_START), indexOfTeacherStopTalking)
+    if (indexOfNextTalkStart === -1) return
 
-    // Check #2: Make sure teacher (eventually) starts talking again
-    if (! doesTeacherTalkResume) { return }
+    // Wait time 1a
+    const is1a = (rows[indexOfNextTalkStart].type === ET.TEACHER_TALK_START)
 
-    const indexOfTeacherTalkStop = _.findIndex(rows, rowTypeIs(EventTypes.TEACHER_TALK_END), i)
-
-    const rowsBetweenTeacherStopAndResumeTalk = _.slice(rows, indexOfTeacherTalkStop, indexOfTeacherTalkResume)
+    const rowsBetweenTeacherStopAndNextTalkStart = _.slice(rows, indexOfTeacherStopTalking, indexOfNextTalkStart)
 
     // Check #3: Make sure there's silence between when teacher stops
-    // talking and when they start talking again
-    if (! isSilentBetween(rowsBetweenTeacherStopAndResumeTalk)) { return }
+    // talking and when the next event occurs
+    if (! isSilentBetween(rowsBetweenTeacherStopAndNextTalkStart)) { return }
 
 
-    if (requireQuestionEvent) {
-      // Find the question event between the start of the teacher
-      // talking and when they start talking again (if one exists).
-      const rowsBetweenTeacherTalking = _.slice(rows, i, indexOfTeacherTalkResume)
-      const question = _.find(rowsBetweenTeacherTalking, rowTypeIs(EventTypes.QUESTION))
+    // // Find the question event between the start of the teacher
+    // // talking and when they start talking again (if one exists).
+    // const rowsWhereQuestionMustOccur = _.slice(rows, i, indexOfNextTalkEnd)
+    // const contentQuestion = _.find(rowsWhereQuestionMustOccur, rowTypeIs(ET.CONTENT_QUESTION))
+    // const nonContentQuestion = _.find(rowsWhereQuestionMustOccur, rowTypeIs(ET.NON_CONTENT_QUESTION))
+    //
+    // // Check #4: Make sure a question was actually asked!
+    // if (!contentQuestion && !nonContentQuestion ) { return }
 
-      // Check #4: Make sure a question was actually asked!
-      if (! question) { return }
-    }
+    let contentQuestion = true
 
-    const momentTalkStopped = moment(rows[indexOfTeacherTalkStop].dateTime)
-    const momentTalkResumed = moment(rows[indexOfTeacherTalkResume].dateTime)
-    waitTimes.push(momentTalkResumed.diff(momentTalkStopped))
+    // console.log(indexOfNextTalkStart)
+    // console.log(indexOfNextTalkEnd)
+    //
+    waitTimes[is1a ? 'a' : 'b'][contentQuestion ? 'content' : 'nonContent'].push(
+      rows[indexOfNextTalkStart].moment.diff(rows[indexOfTeacherStopTalking].moment)
+    )
   })
 
   return waitTimes
 }
 
 function getTalkTimes(rows) {
-
   let allTalkTimes = []
 
   rows.forEach((row, i) => {
-    if (! doesEventTypeBreakSilence(row.type)) {
+    if (!_.includes([ET.TEACHER_TALK_START, ET.STUDENT_TALK_START], row.type)) {
       return
     }
 
-    const isTeacher = (row.type === EventTypes.TEACHER_TALK_START)
+    const isTeacher = (row.type === ET.TEACHER_TALK_START)
     const endEventType = (
-      isTeacher ? EventTypes.TEACHER_TALK_END : EventTypes.STUDENT_TALK_END
+      isTeacher ? ET.TEACHER_TALK_END : ET.STUDENT_TALK_END
     )
     const startEvent = row
     const endEvent = _.find(rows, rowTypeIs(endEventType), i)
     allTalkTimes.push({
       isTeacher,
-      duration: moment(endEvent.dateTime).diff(moment(startEvent.dateTime)),
+      duration: endEvent.moment.diff(startEvent.moment),
     })
   })
 
@@ -104,6 +116,24 @@ function getTalkTimes(rows) {
   }
 }
 
+
+function markSilence(rows) {
+  let isTeacherTalking = false
+  let isStudentTalking = false
+  return _.map(rows, (row, i) => {
+    if (row.type === ET.TEACHER_TALK_START)
+      isTeacherTalking = true
+    else if (row.type === ET.TEACHER_TALK_END)
+      isTeacherTalking = false
+    else if (row.type === ET.STUDENT_TALK_START)
+      isStudentTalking = true
+    else if (row.type === ET.STUDENT_TALK_END)
+      isStudentTalking = false
+    row.isDuringSilence = !isTeacherTalking && !isStudentTalking
+    return row
+  })
+}
+
 function getSilenceStats(rows) {
   let silentSegments = []
   let indexSilenceStarted = 0
@@ -113,19 +143,14 @@ function getSilenceStats(rows) {
   rows.forEach((row, i) => {
     const noOneWasTalking = !isTeacherTalking && !isStudentTalking
     const peopleWereTalking = isTeacherTalking || isStudentTalking
-    if (row.type === EventTypes.TEACHER_TALK_START) {
+    if (row.type === ET.TEACHER_TALK_START)
       isTeacherTalking = true
-    }
-    if (isTeacherTalking && row.type === EventTypes.TEACHER_TALK_END) {
+    else if (row.type === ET.TEACHER_TALK_END)
       isTeacherTalking = false
-    }
-
-    if (row.type === EventTypes.STUDENT_TALK_START) {
+    else if (row.type === ET.STUDENT_TALK_START)
       isStudentTalking = true
-    }
-    if (isStudentTalking && row.type === EventTypes.STUDENT_TALK_END) {
+    else if (row.type === ET.STUDENT_TALK_END)
       isStudentTalking = false
-    }
 
     const nowPeopleAreTalking = isTeacherTalking || isStudentTalking
     const nowNoOneIsTalking = !isTeacherTalking && !isStudentTalking
@@ -144,6 +169,7 @@ function getSilenceStats(rows) {
   const silenceDuration = segment => _.last(segment).moment.diff(_.first(segment).moment)
 
   return {
+    rows: _.flatten(silentSegments),
     count: silentSegments.length,
     sum: millisToRoundedMinutes(_.sum(silentSegments.map(silenceDuration))),
     avg: millisToRoundedMinutes(_.mean(silentSegments.map(silenceDuration))),
@@ -151,8 +177,8 @@ function getSilenceStats(rows) {
 }
 
 const removePauses = rows => F.compose(F.get('list'), F.reduce((acc, row) => {
-  acc.inPause = (row.type === EventTypes.PAUSE_BEGIN)
-  if (!acc.inPause) acc.list.push(row)
+  acc.inPause = (row.type === ET.PAUSE_BEGIN)
+  if (!acc.inPause && row.type !== ET.PAUSE_END) acc.list.push(row)
   return acc
 }, {list: [], inPause: false}))(rows)
 
@@ -161,19 +187,27 @@ const addMoments = F.map(row => _.assign(row, {moment: moment(row.dateTime)}))
 const countRowsOfType = type => F.compose(F.size, F.filter(rowTypeIs(type)))
 
 const calculateStatistics = rows => {
-  rows = F.compose(removePauses, addMoments)(rows)
+  if (_.last(rows)._id !== "") {
+    console.error("bad format")
+    return
+  }
 
-  console.log(countRowsOfType(EventTypes.NAME_USED)(rows))
-  console.log(_.mean(getWaitTimeOnes(rows, true)))
+  // pre-process rows
+  rows = F.compose(
+    markSilence,
+    addMoments,
+    removePauses,
+    F.dropLast(1)
+  )(rows)
 
   return {
-    nameUsedCount: countRowsOfType(EventTypes.NAME_USED)(rows),
-    uniqueStudentTalkCount: countRowsOfType(EventTypes.UNIQUE_STUDENT_TALK)(rows),
-    handsRaisedCount: countRowsOfType(EventTypes.HAND_RAISED)(rows),
-    coldCallsCount: countRowsOfType(EventTypes.COLD_CALL)(rows),
+    nameUsedCount: countRowsOfType(ET.NAME_USED)(rows),
+    uniqueStudentTalkCount: countRowsOfType(ET.UNIQUE_STUDENT_TALK)(rows),
+    handsRaisedCount: countRowsOfType(ET.HAND_RAISED)(rows),
+    coldCallsCount: countRowsOfType(ET.COLD_CALL)(rows),
     silenceStats: getSilenceStats(rows),
     talkTimes: getTalkTimes(rows),
-    waitTimeOnes: getWaitTimeOnes(rows),
+    waitTimeOne: waitTimeOne(rows),
   }
 }
 
